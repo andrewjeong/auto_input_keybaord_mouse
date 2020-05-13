c79277a6968ad3b7ba93ad2cef83bd01a5ff3a82
boyoung <boyn.jeong@gmail.com>
2020-05-13 16:48:38 +0900

    remain time function is added

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pyautogui
+import sys
 import time
 
 def wellformed_time():
@@ -6,20 +7,46 @@ def wellformed_time():
     formed_time = ("%04d/%02d/%02d %02d:%02d:%02d" % (now.tm_year, now.tm_mon, now.tm_mday, now.tm_hour, now.tm_min, now.tm_sec))
     return formed_time
 
-print("Key list: %s", pyautogui.KEYBOARD_KEYS)
+def countdown(max_num):
+    for idx in reversed(range(0, max_num)):
+        time.sleep(1 - time.time() % 1) # sleep until a whole second boundary
+        sys.stdout.write('\r%5d seconds (%02d min %02d sec)' % (idx, idx/60, idx%60))
+    sys.stdout.write('\n')
 
-for idx in range(0, 6):
+def total_remain_time(reamain_cnt, sleep_duration):
+    total_sleep_duration = sleep_duration * max_index * 2
+    sleep_hour = (int) (total_sleep_duration / 3600)
+    sleep_min  = (int) ((total_sleep_duration - (sleep_hour * 3600)) / 60)
+    sleep_sec  = total_sleep_duration % 60
+    print("Total remain time: %02d:%02d:%02d" % (sleep_hour, sleep_min, sleep_sec)) 
+
+# print("Key list: %s", pyautogui.KEYBOARD_KEYS)
+
+
+max_index = 20
+sleep_duration = 540
+total_sleep_duration = sleep_duration * 2 * max_index
+
+for idx in reversed(range(0, max_index)):
+    total_remain_time(idx, sleep_duration)
+    
     # pyautogui.press('hangul')
+    print("%s  index: %s" % (wellformed_time(), idx), flush=True)
+    # pyautogui.press('pause')
+    pyautogui.press('shiftleft')
+    countdown(sleep_duration)
 
-    print("%s  index: %s" % (wellformed_time(), idx))
-    pyautogui.press('pause')
-    time.sleep(590)           # for 9 minutes 50 seconds
-
-    print("%s  index: %s" % (wellformed_time(), idx))
+    print("%s  index: %s" % (wellformed_time(), idx), flush=True)
     pyautogui.press('shiftright')
-    time.sleep(590)           # for 9 minutes 50 seconds
+    countdown(sleep_duration)
 
 
+print("All job is done")
 
+# os.system("taskkill /f /im ttree.exe")
+
+time.sleep(5)
+import ctypes
+ctypes.windll.user32.LockWorkStation()
 
 
